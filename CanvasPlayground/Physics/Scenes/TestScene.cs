f2962107d86783fef81fa66fec4dc0a0a4fcd74f
Thomas <halsvik@hotmail.com>
2016-10-04 22:03:05 +0200

    test scene update

--- CanvasPlayground/Physics/Scenes/TestScene.cs
+++ CanvasPlayground/Physics/Scenes/TestScene.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Timers;
 using CanvasPlayground.Physics.Figures;
+using CanvasPlayground.Utils;
 using Timer = System.Timers.Timer;
 
 namespace CanvasPlayground.Physics.Scenes
@@ -19,19 +20,30 @@ namespace CanvasPlayground.Physics.Scenes
 
         public void Start()
         {
-            Timer t = new Timer();
-            t.Elapsed += T_Elapsed1;
-            t.Interval = 5000;
-            t.Start();
+            SceneTimer.RunSetup(new List<Tuple<int, Action>> {
+                new Tuple<int, Action>(500, () =>{
+                    _worldLoop.AddFigure(new CircleFigure(_worldLoop.World, 75, 400, 400,40));
+                }),
+                new Tuple<int, Action>(1000, () =>{
+                    _worldLoop.AddFigure(new CircleFigure(_worldLoop.World, 75, 600, 400,40));
+                }),
+                new Tuple<int, Action>(1500, () =>{
+                    _worldLoop.AddFigure(new CircleFigure(_worldLoop.World, 75, 600, 600,40));
+                }),
+                new Tuple<int, Action>(2500, () =>{
+                    _worldLoop.AddFigure(new CircleFigure(_worldLoop.World, 75, 400, 600,40));
+                }),
+                new Tuple<int, Action>(3500, () =>{
+                    _worldLoop.AddFigure(new Rectangle(_worldLoop.World, 600,20, 0.2f, 500,500));
+                }),
 
-            _worldLoop.AddFigure(new CircleFigure(_worldLoop.World, 75, 400, 400,40));
+                   new Tuple<int, Action>(10000, () =>{
+                    _worldLoop.AddFigure(new Rectangle(_worldLoop.World, 600,20, 0.2f, 500,500) {Static = true});
+                }),
+            });
+
         }
 
-        private void T_Elapsed1(object sender, ElapsedEventArgs e)
-        {
-            ((Timer)sender).Stop();
-            _worldLoop.AddFigure(new CircleFigure(_worldLoop.World, 250, 400, 400, 40));
-        }
 
         public void Stop()
         {
@@ -45,7 +57,7 @@ namespace CanvasPlayground.Physics.Scenes
                 for (int i = 0; i < 25; i++)
                 {
                     _worldLoop.AddFigure(new Circle(_worldLoop.World, 15, 400, 400));
-                    Thread.Sleep(100);   
+                    Thread.Sleep(100);
                 }
             }
         }
---
+++ CanvasPlayground/Utils/SceneTimer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace CanvasPlayground.Utils
+{
+    public static class SceneTimer
+    {
+
+        public static void RunSetup(List<Tuple<int,Action>> _timeAndScenes)
+        {
+            var list = _timeAndScenes.OrderBy(o => o.Item1);
+
+            foreach (var tuple in list)
+            {
+                var time = tuple.Item1;
+                var action = tuple.Item2;
+                Timer timer = new Timer();
+                timer.Interval = time;
+                timer.Elapsed += (s, e) =>
+                {
+                    timer.Stop();
+                    try
+                    {
+                        action();
+                    }
+                    catch (Exception)
+                    {
+                    }
+                };
+                timer.Start();
+            }
+        }
+    }
+}
